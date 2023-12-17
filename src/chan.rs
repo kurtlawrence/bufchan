e5f024dd5f4798191432318a567ca3b110e39816
Kurt Lawrence <kurtlawrence92@gmail.com>
2023-12-18 07:43:46 +1000

    document items

--- src/chan.rs
+++ src/chan.rs
@@ -1,10 +1,12 @@
 use parking_lot::{Condvar, Mutex};
 use std::{collections::VecDeque, sync::Arc};
 
+/// Create an unbounded channel with default buffer size.
 pub fn unbounded<T>() -> (Sender<T>, Receiver<T>) {
     unbounded_with_buffer(512)
 }
 
+/// Create an unbounded channel with the specified buffer size.
 pub fn unbounded_with_buffer<T>(buf_size: usize) -> (Sender<T>, Receiver<T>) {
     let shared = Arc::new((Mutex::new(Default::default()), Condvar::new()));
     (
@@ -24,6 +26,12 @@ type Shared<T> = Arc<(Mutex<VecDeque<T>>, Condvar)>;
 
 // ###### SENDER ###############################################################
 
+/// The sending side of the channel.
+///
+/// `Sender` is `Send`, `Sync`, and `Clone`able.
+///
+/// When `Sender` is dropped, any locally buffered messages are flushed to the receiver.
+/// Note that this _might_ block if waiting on the shared lock.
 pub struct Sender<T> {
     local: Vec<T>,
     shared: Shared<T>,
@@ -41,6 +49,11 @@ impl<T> Clone for Sender<T> {
 }
 
 impl<T> Sender<T> {
+    /// Buffer a message to be sent.
+    /// If the local buffer reaches the threshold, an attempt to send the buffered messages to the
+    /// receiver occurs.
+    ///
+    /// This is guaranteed not to block.
     pub fn send(&mut self, val: T) {
         // push onto buffer
         self.local.push(val);
@@ -57,13 +70,17 @@ impl<T> Sender<T> {
         }
     }
 
+    /// Flush any locally buffered items to the receiver.
+    ///
+    /// This is blocking.
     pub fn flush(&mut self) {
         if self.local.is_empty() {
             return; // no buffered items, can exit
         }
-        // we have to block until we get the q
         let &(ref lock, ref cvar) = self.shared.as_ref();
+        // we have to block until we get the q
         lock.lock().extend(self.local.drain(..));
+        // notify that the queue has been updated
         cvar.notify_one();
     }
 }
@@ -77,26 +94,42 @@ impl<T> Drop for Sender<T> {
 
 // ###### RECEIVER #############################################################
 
+/// The receiving end of the channel.
+///
+/// There can be only _one_ receiver.
 pub struct Receiver<T> {
     local: VecDeque<T>,
     shared: Shared<T>,
 }
 
 impl<T> Receiver<T> {
+    /// Receive a message, return `None` is there are no more messages _or senders_.
+    ///
+    /// Receive will block until a message becomes available or all associated [`Sender`]s are
+    /// dropped.
     pub fn recv(&mut self) -> Option<T> {
+        // only check for messages if local buffer is empty
         while self.local.is_empty() {
             if Arc::strong_count(&self.shared) == 1 {
+                // there are no more senders, take the shared queue and break
+                // if both queues are empty, breaking will return None and signals
+                // end of channel
                 std::mem::swap(&mut self.local, &mut self.shared.0.lock());
                 break;
             } else {
                 let &(ref lock, ref cvar) = self.shared.as_ref();
+                // obtain a lock on the shared queue
                 let mut q = lock.lock();
                 if q.is_empty() {
+                    // if queue is empty, unlock it and wait for a sender to notify
+                    // that it has been updated
                     cvar.wait_for(&mut q, std::time::Duration::from_millis(100));
                 }
+                // our local buffer is empty, so we can swap the shared one for it
                 std::mem::swap(&mut self.local, &mut q);
             }
         }
+        // pull from the local buffer
         self.local.pop_front()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! A _high throughput_ and _simple_ buffered MPSC channel.
+//!
+//! Take at look at the [README](https://github.com/kurtlawrence/bufchan/blob/main/README.md) for benchmarks and caveats.
+//!
+//! # Example
+//! ```rust
+//! let (mut tx, rx) = bufchan::unbounded();
+//!
+//! std::thread::spawn(move || {
+//!     (1..=10).for_each(|x| tx.send(x));
+//! });
+//!
+//! assert_eq!(
+//!     rx.into_iter().collect::<Vec<_>>(),
+//!     vec![1,2,3,4,5,6,7,8,9,10]
+//! );
+//! ```
 #[cfg(test)]
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
